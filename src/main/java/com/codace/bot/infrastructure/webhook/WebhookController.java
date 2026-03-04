097ceddae58e58bb5d317f8517925718c90cc7ac
Mota <thiago.motahonda11@gmail.com>
2026-03-04 16:04:28 -0300

    feat(webhook): add dynamic greeting based on time of day and spring devtools

--- src/main/java/com/codace/bot/infrastructure/webhook/WebhookController.java
+++ src/main/java/com/codace/bot/infrastructure/webhook/WebhookController.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalTime;
+
 @RestController
 @RequestMapping("/webhook")
 public class WebhookController {
@@ -16,9 +18,22 @@ public class WebhookController {
         this.wahaClient = wahaClient;
     }
 
+    private String getGreeting() {
+        int hour = LocalTime.now().getHour();
+
+        if (hour >= 6 && hour < 12) {
+            return "Bom dia";
+        } else if (hour >= 12 && hour < 18) {
+            return "Boa tarde";
+        } else {
+            return "Boa noite";
+        }
+    }
+
     @PostMapping("/waha")
     public void receiveMessage(@RequestBody WahaWebhookEvent event) {
         log.info("📩 Received webhook event: {}", event.getEvent());
+
 
         if ("message".equals(event.getEvent())) {
             String from = event.getPayload().getFrom();
@@ -26,12 +41,14 @@ public class WebhookController {
 
             log.info("💬 Message from: {} - Text: {}", from, text);
 
-            // Responder com "Echo: " + mensagem recebida
             if (text != null && !text.isEmpty()) {
-                String response = "Olá! Sou o Alfred \uD83E\uDD16 \uD83D\uDC54, seu assistente financeiro virtual " + text;
+                String greeting = getGreeting();
+
+                String response = greeting + " senhor, Alfred seu mordomo digital e guardião dos seus ativos financeiros à sua disposição " + "\uD83D\uDC54 \u2615\ufe0f";
                 wahaClient.sendTextMessage(from, response);
                 log.info("✅ Response sent: {}", response);
             }
         }
     }
 }
+
